3115ba80936a55e997b76c6f647f4bbf7d26e635
Germano <germano_gomes@estudante.sesisenai.org.br>
2025-03-11 21:52:44 -0300

    Semana06 react2 11/03/25

--- semana06/src/app/login/page.tsx
+++ semana06/src/app/login/page.tsx
@@ -1,13 +1,54 @@
+'use client'
+
+import './styles.css'
 import Link from "next/link";
+import { useRouter } from 'next/navigation';
+import { useState } from "react";
 
 export default function login() {
-    return (
-        <div>
-            <h1>Login</h1>
-            <Link href="/">
-                <button>Home</button>
-            </Link>
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassworld] = useState<string>('')
 
+    const router = useRouter()
+
+    function handleSubmit() {
+     
+        console.log(email)
+        console.log(password)
+
+
+
+
+        router.replace("/")
+    }
+
+
+    return (
+        <div className="container">
+            <div className="form">
+                <h2>Login</h2>
+
+                <input
+                    type="text"
+                    placeholder="E-mail"
+                    className="input"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="input"
+                    value={password}
+                    onChange={(event) => setPassworld(event.target.value)}
+                />
+
+                <button
+                    className="button" onClick={handleSubmit} disabled ={!email || !password}>
+                    Entrar
+                </button>
+            </div>
         </div>
     )
 }
--- semana06/src/app/page.tsx
+++ semana06/src/app/page.tsx
@@ -1,49 +1,83 @@
-import Link from "next/link";
+'use client'
+
 import "./home.css";
+import Link from "next/link";
+import { useState } from "react";
 import Person from "@/components/person";
 
-export default function Home() {
-  const list = [
-    {
-      name: "Germano",
-      age: 18
-    },
-    {
-      name: "Lewis",
-      age: 28
-    },
-    {
-      name: "João",
-      age: 39
-    },
-    {
-      name: "Kyra",
-      age: 38
-    },
-    {
-      name: "Sebastian",
-      age: 32
-    }
-  ]
+export default function Home() {  
+  const [contador,setContador] = useState<number>(0)
 
+  function incrementar(){
+    setContador(contador +1)
+  }
+  function resetar(){
+   setContador(0)
+  }
+  
+  
+  
+  
+  
+  
+  
   return (
-    <div>
-      <h1>Hello World</h1>
-      <h2>hi</h2>
-
-      <Link href="/login">
-        <button>Login</button>
-      </Link>
-
-      <div className="container">
-        {
-          list.map((item) => (
-            <Person nome={item.name} idade={item.age} />
-          ))
-        }
+      <div>
+          <h1>Contador: {contador}</h1>
+          <button onClick={incrementar}>INCREMENTAR</button>
+          <button onClick={resetar}>RESETAR</button>
       </div>
-
-    </div>
-
-  );
+    
+  
+    
+    );
 }
+      
+      
+      
+      
+      
+      
+      
+      
+      // <div>
+      //   <h1>Hello World</h1>
+      //   <h2>hi</h2>
+      
+      //   <Link href="/login">
+      //     <button>Login</button>
+      //   </Link>
+      
+      //   <div className="container">
+      //     {
+        //       list.map((item) => (
+          //         <Person nome={item.name} idade={item.age} key={item.name}/>
+          //       ))
+          //     }
+          //   </div>
+          
+          // </div>
+      
+//fora do return--------------------------------------------------------------------------
+      // const list = [
+      //   // {
+      //   //   name: "Germano",
+      //   //   age: 18
+      //   // },
+      //   // {
+      //   //   name: "Lewis",
+      //   //   age: 28
+      //   // },
+      //   // {
+      //   //   name: "Jhon",
+      //   //   age: 56
+      //   // },
+      //   // {
+      //   //   name: "Kyra",
+      //   //   age: 38
+      //   // },
+      //   // {
+      //   //   name: "Sebastian",
+      //   //   age: 32
+      //   // }
+      // ]
