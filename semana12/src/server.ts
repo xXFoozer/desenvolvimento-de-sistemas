3a2f5bcc9e33892393903f72aa0d9e62f3da9d7f
GERMANO RODRIGUES GOMES <germano_gomes@estudante.sesisenai.org.br>
2025-04-28 22:02:29 -0300

    Semana 12 BACKEND 01

---
+++ semana12/src/server.ts
@@ -0,0 +1,16 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/hello", ()=>{
+    console.log("Entrou no end point Hello!")
+    return "Olá Mundo, Boa noite"
+})
+
+
+
+
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('Backend rodando na porta 3333')
+})
